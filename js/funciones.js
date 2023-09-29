9e28c3eeff23a31fdd222876851c87fa7b2c95df
kaiannlobo <kaiannlobo@gmail.com>
2023-09-29 14:39:30 +0200

    cambios front

---
+++ js/funciones.js
@@ -0,0 +1,13 @@
+(async() => {
+
+        try{
+                let {r,g,b} = await fetch("https://prueba-backend-z6fq.onrender.com/").then(respuesta => respuesta.json());
+
+                document.querySelector("body").style.backgroundColor = `rgb(${[r,g,b].join(" , ")})`;
+                document.querySelector("h1").innerHTML = [r,g,b].join(", ");
+
+        }catch(error){
+            document.querySelector("h1").innerHTML = "ERROR"
+
+        }
+})();
